e823362c13ed5c8d4006b489dee9dee1f2c0321e
Mojken <mikaelgillis@gmail.com>
2016-09-20 21:28:29 +0200

    Changed how projectiles move

--- src/main/Update.java
+++ src/main/Update.java
@@ -5,9 +5,6 @@ import java.awt.event.KeyEvent;
 import java.io.IOException;
 import java.util.ArrayList;
 import javax.imageio.ImageIO;
-
-import com.sun.javafx.scene.control.skin.VirtualFlow.ArrayLinkedList;
-
 import net.abysmal.engine.graphics.Graphics;
 import net.abysmal.engine.graphics.Window;
 import net.abysmal.engine.graphics.geometry.Square;
@@ -33,10 +30,8 @@ public class Update implements Tick {
 	@Override
 	public void update() {
 		if (Main.initialized) {
-			if (main.w.keyboardListener.getCurrentlyPressedKeys()[KeyEvent.VK_CONTROL][0] == 1)
-				Main.selectedBuildingType = 0;
-			else
-				Main.selectedBuildingType = 1;
+			if (main.w.keyboardListener.getCurrentlyPressedKeys()[KeyEvent.VK_CONTROL][0] == 1) Main.selectedBuildingType = 0;
+			else Main.selectedBuildingType = 1;
 			if (main.w.mouseListener.getClickInfo()[1][4] == 1 && (!lastInput || main.w.keyboardListener.getCurrentlyPressedKeys()[KeyEvent.VK_SHIFT][0] == 1)) {
 				if (main.track.isWithin(main.w.mouseListener.getMousePosition())) {
 					// if(buildingSelected){
@@ -50,15 +45,12 @@ public class Update implements Tick {
 					if (main.w.keyboardListener.getCurrentlyPressedKeys()[KeyEvent.VK_CONTROL][0] == 1) {
 						Building b0 = Main.currentTrack.buildings[Main.currentTrack.grid.getGridIndex(main.w.mouseListener.getMousePosition().sub(main.track.a))];
 						if (b0 != null) {
-							for (Building b1 : Main.currentTrack.buildings) {
-								if (b1 == null)
-									continue;
-								if (b0.ID == b1.ID && b0.type == b1.type)
-									Building.sellBuilding(Main.currentTrack.grid.getGridCoordinate(b1.gridIndex));
+							for (Building b1:Main.currentTrack.buildings) {
+								if (b1 == null) continue;
+								if (b0.ID == b1.ID && b0.type == b1.type) Building.sellBuilding(Main.currentTrack.grid.getGridCoordinate(b1.gridIndex));
 							}
 						}
-					} else
-						Building.sellBuilding(main.w.mouseListener.getMousePosition().sub(main.track.a));
+					} else Building.sellBuilding(main.w.mouseListener.getMousePosition().sub(main.track.a));
 					// }
 				}
 			}
@@ -69,7 +61,7 @@ public class Update implements Tick {
 
 			if (shoot || skip) {
 				if (!reading) {
-					for (Building b : Main.currentTrack.buildings) {
+					for (Building b:Main.currentTrack.buildings) {
 						if (b instanceof Block && ((Block) b).tower != null) {
 							Tower source = ((Block) b).tower;
 							Vector target = main.w.mouseListener.getMousePosition();
@@ -86,12 +78,10 @@ public class Update implements Tick {
 			}
 
 			lastInput = main.w.mouseListener.getClickInfo()[1][4] == 1;
-			if (shot > 0)
-				shot++;
-			if (shot >= 100)
-				shot = 0;
+			if (shot > 0) shot++;
+			if (shot >= 100) shot = 0;
 
-			for (Projectile p : Projectile.projectiles) {
+			for (Projectile p:Projectile.projectiles) {
 				// System.out.println(p.target);
 				p.move();
 			}
@@ -103,52 +93,47 @@ public class Update implements Tick {
 		if (Main.initialized) {
 			Graphics g = new Graphics(g2);
 			g2.clearRect(0, 0, Window.width, Window.height);
-			// super.paint(g);
-			for (Square p : main.partition.partitions) {
+			for (Square p:main.partition.partitions) {
 				g.drawRect(p.a, p.b);
 			}
+			drawGame(g);
 			g2.drawString("Money: " + Player.money, (int) main.money.a.x, (int) main.money.b.y);
 			g2.drawString("Research: " + Player.research, (int) main.research.a.x, (int) main.research.b.y);
-			g.drawImage(Main.currentTrack.world.world, main.track.a, main.track.b.sub(main.track.a));
-			g.setColor(new Color(0x10303030, true));
-			for (Square p : Main.currentTrack.grid.grid) {
-				g.drawRect(p.a.add(main.track.a), p.b.add(main.track.a));
-			}
-			for (Building b : Main.currentTrack.buildings) {
-				if (b == null)
-					continue;
-				try {
-					Vector pos = Main.currentTrack.grid.getGridCoordinate(b.gridIndex).add(main.track.a);
-					g.drawImage(ImageIO.read(b.getFile()), pos, Main.currentTrack.tileSize.toVector());
-					if (b instanceof Block && ((Block) b).tower != null) {
-						g.drawImage(ImageIO.read(((Block) b).tower.getFile()), pos, Main.currentTrack.tileSize.toVector());
-					}
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-			}
-			drawOnHover(g);
-			reading = true;
-			for (Projectile p : Projectile.projectiles) {
-				if (p == null)
-					continue;
-				try {
-					if (null == p.textureURL || p.pos == null)
-						continue;
-					g.drawImage(ImageIO.read(p.textureURL), p.pos.add(p.hitbox.getHitboxPoints()[0].multiply(2.5f)));
-				} catch (IOException e) {
-					System.out.println("Projectile image failed to load");
-					e.printStackTrace();
-				}
-			}
-			reading = false;
 			g.dispose();
 		}
 	}
 
+	private void drawGame(Graphics g) {
+		g.drawImage(Main.currentTrack.world.world, main.track.a, main.track.b.sub(main.track.a));
+		for (Building b:Main.currentTrack.buildings) {
+			if (b == null) continue;
+			try {
+				Vector pos = Main.currentTrack.grid.getGridCoordinate(b.gridIndex).add(main.track.a);
+				g.drawImage(ImageIO.read(b.getFile()), pos, Main.currentTrack.tileSize.toVector());
+				if (b instanceof Block && ((Block) b).tower != null) {
+					g.drawImage(ImageIO.read(((Block) b).tower.getFile()), pos, Main.currentTrack.tileSize.toVector());
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		drawOnHover(g);
+		reading = true;
+		for (Projectile p:Projectile.projectiles) {
+			if (p == null) continue;
+			try {
+				if (null == p.textureURL || p.pos == null) continue;
+				g.drawImage(ImageIO.read(p.textureURL), p.pos.add(p.hitbox.getHitboxPoints()[0].multiply(2.5f)));
+			} catch (IOException e) {
+				System.out.println("Projectile image failed to load");
+				e.printStackTrace();
+			}
+		}
+		reading = false;
+	}
+
 	public void drawOnHover(Graphics g) {
 		if (main.track.isWithin(main.w.mouseListener.getMousePosition())) {
-
 			Vector tile = Main.currentTrack.grid.getGridCoordinate(main.w.mouseListener.getMousePosition().sub(main.track.a)).add(main.track.a);
 			if (!(Main.selectedBuildingID < 0) && Block.blocks.size() > Main.selectedBuildingID) {
 				int gridIndex = Main.currentTrack.grid.getGridIndex(Main.currentTrack.grid.getGridCoordinate(main.w.mouseListener.getMousePosition().sub(main.track.a)));
@@ -160,19 +145,14 @@ public class Update implements Tick {
 									g.setColor(new Color(0x50202040, true));
 								} else if (Main.selectedBuildingType == 1 && Main.currentTrack.buildings[gridIndex] == null) {
 									g.setColor(new Color(0x50202040, true));
-								} else
-									g.setColor(new Color(0x70FF0000, true));
-							} else
-								g.setColor(new Color(0x70FF0000, true));
-						} else
-							g.setColor(new Color(0x70FF0000, true));
-					} else
-						g.setColor(new Color(0x70FF0000, true));
-				} else
-					g.setColor(new Color(0x00FF0000, true));
-			} else
-				g.setColor(new Color(0x70FF0000, true));
+								} else g.setColor(new Color(0x70FF0000, true));
+							} else g.setColor(new Color(0x70FF0000, true));
+						} else g.setColor(new Color(0x70FF0000, true));
+					} else g.setColor(new Color(0x70FF0000, true));
+				} else g.setColor(new Color(0x00FF0000, true));
+			} else g.setColor(new Color(0x70FF0000, true));
 			g.fillRect(tile, Main.currentTrack.tileSize.toVector().add(tile));
+			g.setColor(Color.BLACK);
 		}
 	}
 }
--- src/objects/entities/Projectile.java
+++ src/objects/entities/Projectile.java
@@ -1,7 +1,6 @@
 package objects.entities;
 
 import java.util.ArrayList;
-
 import net.abysmal.engine.handlers.misc.Movement;
 import net.abysmal.engine.maths.Hitbox;
 import net.abysmal.engine.maths.Vector;
@@ -18,20 +17,23 @@ public class Projectile extends net.abysmal.engine.entities.Projectile<Tower>{
 		super(id, speed, 0, damage, texture, hitbox);
 	}
 	
-	public Projectile(Vector pos, Projectile type, Tower source, Vector target){
+	public Projectile(Vector pos, Projectile type, Tower source, float angle){
 		super(pos, type, source);
-		this.target = target;
+		this.angle = angle;
 	}
 	
 	public void shoot(Vector pos, Tower source, Vector target) {
-		projectiles.add(new Projectile(pos, this, source, target));
+		target = target.sub(pos);
+		angle = (float) (java.lang.Math.atan(target.getX() / target.getY()) + rotation);
+		if (target.getY() != java.lang.Math.abs(target.getY()))
+			angle += net.abysmal.engine.maths.Math.TAU / 2;
+		projectiles.add(new Projectile(pos, this, source, angle));
 	}
 	
 	
 	
 	public void move(){
-		if(target != null)
-		Movement.walkToVectorWithRotation(target, this, 0, speed);
+		Movement.moveInAngleWithRotation(angle, this, 0, speed);
 	}
 	
 }
